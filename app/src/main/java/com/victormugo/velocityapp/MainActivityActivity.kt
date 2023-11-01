98f29362276429de863411a30d55822e6325be9f
Victor <victormugo@gmail.com>
2023-11-01 10:06:32 +0100

    firts commit

---
+++ app/src/main/java/com/victormugo/velocityapp/MainActivityActivity.kt
@@ -0,0 +1,31 @@
+package com.victormugo.velocityapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
---
+++ app/src/main/java/com/victormugo/velocityapp/services/VelocityService.kt
@@ -0,0 +1,12 @@
+package com.victormugo.velocityapp.services
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class VelocityService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
